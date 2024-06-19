1c7d852f340311aa9686e50665079b13259423ce
prakashb-ai <prakashbodicse@gmail.com>
2024-06-19 22:48:16 +0530

    add

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
 import Login from '../images/Login.jpg'
@@ -6,17 +6,72 @@ import Login from '../images/Login.jpg'
 
 function LoginPage() {
 
+
     const navigate = useNavigate();
 
-    const handleLogin = () => {
-        navigate('/home');
-      };
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
-    const RegisterLogin = ()=>{
+
+
+    const handleLogin = async () => {
+        if (!email.trim() && !password.trim()) {
+            setError('Please fill in both fields.');
+            return;
+        }
+        else if(!email.trim()){
+            setError('Please fill in email fields.');
+            return;
+        }
+        else if( !password.trim()){
+            setError('Please fill in password fields.');
+            return
+        }
+
+
+        setLoading(true);
+        setError('');
+        try {
+            const response = await fetch('http://localhost:8000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify({ email, password })
+            });
+            if (!response.ok) {
+                throw new Error('Login Failed')
+            }
+            const data = await response.json()
+            console.log('Login successful', data)
+
+            navigate('/home');
+
+        } catch (error) {
+            setError('Error logging in: ' + error.message);
+
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+    useEffect(() => {
+        handleLogin();
+    })
+
+
+
+    const RegisterLogin = () => {
         navigate('/register')
     }
+
+
     return (
+
         <div className='container-fluid'>
+            
             <div className='container mt-5'>
                 <div className='row'>
                     <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6'>
@@ -29,28 +84,78 @@ function LoginPage() {
                             <h1 className='d-flex justify-content-center'>Login Page</h1>
                         </div>
 
+
                         <div className='m-2'>
                             <div className='row d-flex justify-content-center'>
-                                <input name='email'  type='text' placeholder='enter a email' className='p-4 m-3 border border-success' style={{ height: '45px', width: '60%', borderRadius: '50px' }} />
-                                <input name='password' type='password' placeholder='enter a password' className='p-4 m-2 border border-success' style={{ height: '45px', width: '60%', borderRadius: '50px' }} />
+
+                                <input name='email'
+                                    type='text'
+                                    placeholder='enter a email'
+                                    className='p-4 m-3 border border-success'
+                                    value={email}
+                                    onChange={(e) => {
+                                        setEmail(e.target.value);
+                                        setError('');
+                                    }}
+                                    style={{
+                                        height: '45px',
+                                        width: '60%',
+                                        borderRadius: '50px'
+                                    }} />
+
+                                <input name='password'
+                                    type='password'
+                                    placeholder='enter a password'
+                                    className='p-4 m-2 border border-success'
+                                    value={password}
+
+                                    onChange={(e) => {
+                                        setPassword(e.target.value);
+                                        setError('');
+                                    }}
+                                    style={{
+                                        height: '45px',
+                                        width: '60%',
+                                        borderRadius: '50px'
+                                    }} />
                             </div>
                         </div>
 
-                    
+
                         <div className='m-3 d-flex justify-content-center'>
-                           <button className='border border-success bg-white' onClick={handleLogin} style={{width:'35%', height:'10%', borderRadius:'10px'}}>
-                                Login
-                           </button>
-                           <button className='border border-success bg-white mx-4' onClick={RegisterLogin} style={{width:'35%', height:'10%', borderRadius:'10px'}}>
+                            <button className='border border-success bg-white'
+                                onClick={handleLogin}
+                                disabled={loading}
+
+                                style={{
+                                    width: '35%',
+                                    height: '10%',
+                                    borderRadius: '10px'
+                                }}>
+                                {loading ? 'Login' : 'Login'}
+
+
+                            </button>
+
+                            <button className='border border-success bg-white mx-4'
+                                onClick={RegisterLogin}
+                                style={{ width: '35%', height: '10%', borderRadius: '10px' }}>
                                 Register
-                           </button>
+                            </button>
 
                         </div>
 
-                        
+
                     </div>
                 </div>
 
+            </div>
+            <div className='col-sm-12 col-md-12 col-xl-12 col-lg-12 d-flex justify-content-end'>
+            {error && (
+                <div className='alert alert-danger m-1 d-flex justify-content-center  ' style={{width:'250px',height:'50px', borderRadius:'6px'}}>
+                    {error}
+                </div>
+            )}
             </div>
         </div>
     )
